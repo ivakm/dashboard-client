1bd577c56be2c0f4ae1016dc47a45663ae082d90
ivakm <dimashevchenko594@gmail.com>
2020-04-21 13:20:35 +0300

    added angular-materail lib created simply order's form

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      name: [''],
+      lastName: [''],
+      tel: ['']
+    });
+
+  }
+
+  submitForm() {
+    console.log(this.form.value)
   }
 
 }
--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 
 import { DashboardComponent } from "./dashboard.component";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonModule } from "@angular/material/button";
+
 
 @NgModule({
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   declarations: [DashboardComponent],
 })
 export class DashboardModule {
