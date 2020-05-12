226bac7773a492a617e8d8300a1c0c4b1d28bf86
ivakm <sheva594@ukr.net>
2020-05-12 19:29:46 +0300

    added Docker and docker ignore

---
+++ src/app/websocket/index.ts
@@ -0,0 +1,7 @@
+export * from './websocket.module';
+export * from './websocket.config';
+export * from './websocket.interface';
+export * from './websocket.service';
+
+
+// https://github.com/AlexDaSoul/angular-websocket-starter
